99f7557aaaec42552f984b7fd926b285de3b9335
김세영 <yy0867@gmail.com>
2022-06-20 14:18:08 +0900

    add: Logger 추가

---
+++ Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  NetworkKit
+//
+//  Created by 김세영 on 2022/06/20.
+//
+
+import Foundation
+import OSLog
+
+public final class Log {
+    
+    private static let logger = Logger()
+    
+    static func log(_ message: String, level: OSLogType = .default) {
+        logger.log(level: level, "\(message)")
+    }
+}
