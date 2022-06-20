06b967e158f857957f34a2d86e1221be94530218
김세영 <yy0867@gmail.com>
2022-06-20 13:58:26 +0900

    initial commit

---
+++ Pullgo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pullgo/PullgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullgoApp.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct PullgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
