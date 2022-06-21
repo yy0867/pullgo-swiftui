868bef540263b0702b9046d39f88a38d28d4b9ca
김세영 <yy0867@gmail.com>
2022-06-21 14:46:45 +0900

    add: UserSessionDataStore 및 Protocol 추가

---
+++ PullgoKit/DataStore/UserSession/UserSessionDataStore.swift
@@ -0,0 +1,82 @@
+//
+//  UserSessionDataStore.swift
+//  PullgoKit
+//
+//  Created by 김세영 on 2022/06/20.
+//
+
+import Foundation
+import Combine
+import NetworkKit
+import TokenKit
+
+public final class UserSessionDataStore: UserSessionDataStoreProtocol {
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    public func signIn(credential: Credential) -> AnyPublisher<UserSession, Error> {
+        var endpoint = Endpoint(httpMethod: .post, paths: ["auth", "token"])
+        endpoint.setHTTPBody(credential)
+        
+        return NetworkSession.shared.request(endpoint.toURLRequest())
+            .decode(type: UserSession.self, decoder: JSONDecoder())
+            .mapError(mapSignInFailError)
+            .eraseToAnyPublisher()
+    }
+    
+    public func rememberMe() -> AnyPublisher<UserSession?, Never> {
+        let tokenManager = KeychainTokenManager()
+        guard let token = tokenManager.readToken() else {
+            return Result.Publisher(nil)
+                .eraseToAnyPublisher()
+        }
+        
+        var endpoint = Endpoint(paths: ["auth", "me"])
+        endpoint.setAuthorization(with: token)
+        
+        return NetworkSession.shared.request(endpoint.toURLRequest())
+            .decode(type: UserSession?.self, decoder: JSONDecoder())
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
+    }
+    
+    public func signUp(student: Student) -> AnyPublisher<Student, Error> {
+        var endpoint = Endpoint(httpMethod: .post, paths: ["students"])
+        endpoint.setHTTPBody(student)
+        
+        return NetworkSession.shared.request(endpoint.toURLRequest())
+            .decode(type: Student.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    public func signUp(teacher: Teacher) -> AnyPublisher<Teacher, Error> {
+        var endpoint = Endpoint(httpMethod: .post, paths: ["teachers"])
+        endpoint.setHTTPBody(teacher)
+        
+        return NetworkSession.shared.request(endpoint.toURLRequest())
+            .decode(type: Teacher.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    public func signOut() -> AnyPublisher<Bool, Never> {
+        let tokenManager = KeychainTokenManager()
+        
+        return Result.Publisher(tokenManager.delete())
+            .eraseToAnyPublisher()
+    }
+    
+    // MARK: - Privates
+    private func mapSignInFailError(_ error: Error) -> Error {
+        guard let error = error as? NetworkError else {
+            return error
+        }
+        
+        if case .invalidCode(let code) = error,
+           code == 401 {
+            return PullgoError.signInFail
+        } else {
+            return error
+        }
+    }
+}
--- PullgoKit/Model/PullgoError.swift
+++ PullgoKit/Model/PullgoError.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import NetworkKit
 
 public enum PullgoError: Error {
     case unknown
