095556f150019b5e811a374e4d5a471a5d1593cc
김세영 <yy0867@gmail.com>
2022-06-21 21:47:12 +0900

    add: LaunchView + ViewModel 추가

---
+++ Pullgo/App/Launch/LaunchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchViewModel.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2022/06/21.
+//
+
+import Foundation
+import Combine
+
+final class LaunchViewModel: ObservableObject {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Methods
+    
+}
