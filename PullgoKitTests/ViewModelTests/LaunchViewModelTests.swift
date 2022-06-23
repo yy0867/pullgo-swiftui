b1ae123195b7071d21369b564f83da5768eff9d6
김세영 <yy0867@gmail.com>
2022-06-23 16:07:24 +0900

    add: FailLaunchViewModel 테스트 추가

--- PullgoKitTests/ViewModelTests/LaunchViewModelTests.swift
+++ PullgoKitTests/ViewModelTests/LaunchViewModelTests.swift
@@ -73,5 +73,54 @@ class SucceedLaunchViewModelTests: XCTestCase {
 ///   - 토큰 load 실패, 혹은 토큰은 load했지만 `UserSession`을 로드하는 데에 실패하면
 ///   `AppState`가 `.notAuthenticated`이 되는지 확인
  class FailLaunchViewModelTests: XCTestCase {
+     
+     var viewModel: LaunchViewModel!
+     var cancellables = Set<AnyCancellable>()
+     
+     override func setUp() {
+         super.setUp()
+         self.viewModel = LaunchViewModel(
+            userSessionRepository: TestInstance.shared.failUserSessionRepository
+         )
+     }
+     
+     override func tearDown() {
+         super.tearDown()
+         self.viewModel = nil
+         self.cancellables.removeAll()
+     }
     
+     func test_LaunchViewModel_loadUserSession_shouldSetAppStateLaunching() {
+         // Given
+         let expectation = self.expectation()
+         
+         // When
+         var appState: AppState?
+         viewModel.$appState
+             .sink(
+                receiveValue: { receivedAppState in
+                    appState = receivedAppState
+                    if case .running(.notAuthenticated) = receivedAppState {
+                        expectation.fulfill()
+                    }
+                }
+             )
+             .store(in: &cancellables)
+         
+         viewModel.loadUserSession()
+         
+         // Then
+         wait(for: [expectation], timeout: 3)
+         XCTAssertNotNil(appState)
+         switch appState! {
+             case .running(let authenticationState):
+                 if case .notAuthenticated = authenticationState {
+                     return
+                 } else {
+                     XCTFail("expecting app state is running - notAuthenticated.")
+                 }
+             case .launching:
+                 XCTFail("expecting app state is running - notAuthenticated.")
+         }
+     }
 }
