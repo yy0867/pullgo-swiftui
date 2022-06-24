b72c42860d34ef6bf2f5cadb93b4294de7e6904e
김세영 <yy0867@gmail.com>
2022-06-24 15:02:54 +0900

    add: OnboardingView 추가

---
+++ Pullgo/App/Running/Onboarding/OnboardingView.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingView.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2022/06/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - Properties
+    
+    // MARK: - UI
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
